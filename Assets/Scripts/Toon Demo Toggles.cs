ced1c851419ab63cd4373f5ce5c356ef3b245290
NeoThePotato <neorayb@gmail.com>
2025-01-04 21:58:53 +0200

    Bind toggles for DOTween animations.

--- Assets/Scripts/Toon Demo Toggles.cs
+++ Assets/Scripts/Toon Demo Toggles.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Linq;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class ToonDemoToogles : MonoBehaviour
 {
     private const string BLINN_PHONG_PROPERTY = "_BLINN_PHONG";
 	[SerializeField] private Material[] _toonMaterials;
+	[SerializeField] private string _rotateID, _colorID;
 	private LocalKeyword _blinnPhongKeyword;
 
 	private void Awake()
@@ -15,4 +17,16 @@ public class ToonDemoToogles : MonoBehaviour
 	}
 
 	public void SetBlinnPhong(bool value) => Array.ForEach(_toonMaterials, m => m.SetKeyword(_blinnPhongKeyword, value));
+
+	public void SetRotationAnimation(bool value) => SetDOTweenPlay(_rotateID, value);
+
+	public void SetColorAnimation(bool value) => SetDOTweenPlay(_colorID, value);
+
+	private static void SetDOTweenPlay(string id, bool value)
+	{
+		if (value)
+			DOTween.Play(id);
+		else
+			DOTween.Pause(id);
+	}
 }
